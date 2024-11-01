1bad184810a291f5d029979433c6da57698ce404
sajidfardannuazhar <sfardan321@gmail.com>
2024-11-01 16:14:45 +0700

    penambahan vercel lagi capek.json

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/format.css', 'resources/js/app.js'], // Pastikan ini sesuai dengan struktur Anda
+            input: ['public/css/format.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    build: {
-        outDir: 'public/dist', // Sesuaikan output jika diperlukan
-    },
 });
